2265e580e458ef5879dd0199c42513ded44a6809
IGrock88 <igrock88@gmail.com>
2018-03-04 19:14:00 +0800

    *fix title *add log out button

--- templates/adminBase.php
+++ templates/adminBase.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
+    <title>Здравствуй хозяина!</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
@@ -25,7 +25,7 @@
         <div class="dropdown account">
             <div class="dropdown-trigger">
                 <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                    <span><?php echo $_SESSION['login'] = "admin"?></span>
+                    <span><?php echo $_SESSION['login']?></span>
                     <span class="icon is-small">
                         <i class="fas fa-angle-down" aria-hidden="true"></i>
                     </span>
@@ -34,21 +34,11 @@
             <div class="dropdown-menu" id="dropdown-menu" role="menu">
                 <div class="dropdown-content">
                     <a href="#" class="dropdown-item">
-                        Dropdown item
+                        Кабинет
                     </a>
-                    <a class="dropdown-item">
-                        Other dropdown item
-                    </a>
-                    <a href="#" class="dropdown-item is-active">
-                        Active dropdown item
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        Other dropdown item
-                    </a>
-                    <hr class="dropdown-divider">
-                    <a href="#" class="dropdown-item">
-                        With a divider
-                    </a>
+                    <form action="/" method="post" class="dropdown-item">
+                        <input class="exitAcount" type="submit" name="ExitLogin" value="log out">
+                    </form>
                 </div>
             </div>
         </div>

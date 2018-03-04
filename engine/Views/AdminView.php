b7a90c2181166733efeac0856ee26a51a5573f91
IGrock88 <igrock88@gmail.com>
2018-03-04 19:20:11 +0800

    * add variable with base admin template * function for output the admin view

--- engine/Views/AdminView.php
+++ engine/Views/AdminView.php
@@ -6,8 +6,10 @@ namespace engine\Views;
 class AdminView extends AbstractView
 {
 
+    private $basisPage = "templates/adminBase.php";
+
     public function generate($content)
     {
-
+        include $this->basisPage;
     }
 }

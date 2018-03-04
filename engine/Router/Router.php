bff3ef2142835617358cea1977a922e0ef033d25
IGrock88 <igrock88@gmail.com>
2018-03-04 22:04:29 +0800

    delete comments

--- engine/Router/Router.php
+++ engine/Router/Router.php
@@ -8,14 +8,6 @@ use engine\components\Singleton;
 class Router
 {
     use Singleton;
-//    private $routing = [
-//        "/"      => ['controller' => "MainController", 'method' => 'index'],
-//        "/goods" => ['controller' => "GoodsController", 'method' => 'goods'],
-//        "/ajaxFeatureGoods" => ['controller' => "GoodsController", 'method' => 'featureGoods'],
-//        "/register" => ['controller' => "UserController", 'method' => 'registration'],
-//        "/account" => ['controller' => "UserController", 'method' => 'account'],
-//        "/admin" => ['controller' => "AdminController", 'method' => 'index']
-//    ];
 
     private $controllers = [
         ""      => ['controller' => "MainController"],
@@ -46,20 +38,12 @@ class Router
     public function start()
     {
         $route = explode("/", $_SERVER['REQUEST_URI']);
-
         $controller = $this->controllers[$route[1]]['controller'];
         $method = $this->methods[$controller][$route[2]];
 
         if(isset($controller) && isset($method)){
-
-
-
             $controller = 'engine\\Controllers\\' . $controller;
-
-//            $controller = 'engine\\Controllers\\' . $this->routing[$route]['controller'];
             $controllerObject = new $controller();
-//            $method = $this->routing[$route]['method'];
-
             $controllerObject->$method();
         }
         else{

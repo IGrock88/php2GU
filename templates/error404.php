9e39b40eca6286d5b5652abd31186f33ee2b0fb8
IGrock88 <igrock88@gmail.com>
2018-03-04 19:14:58 +0800

    minor fix

--- templates/error404.php
+++ templates/error404.php
@@ -1,6 +1,7 @@
 <main class="container" style="text-align: center">
-    <h1>ERROR 404 Страница не существует. Но... КОТИКИ существуют))</h1>
+    <h1>ERROR 404 Страница не существует. Но... <br><br>КОТИКИ существуют)) <br><br>ВАУ!!!</h1>
 
     <img src="../img/error404.jpg" alt="">
-    <a href="/" style="display: block"><span>Перейти на главную страницу</span></a>
+    <br>
+    <h2><a href="/" style="display: block">Перейти на <span class="toMain">главную страницу</span></a></h2>
 </main>

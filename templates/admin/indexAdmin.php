ef046475cbccca21dd853fa535ceb15c7a74aa0c
IGrock88 <igrock88@gmail.com>
2018-03-05 23:29:36 +0800

    admin templates

---
+++ templates/admin/indexAdmin.php
@@ -0,0 +1,9 @@
+<article class="tile is-child notification is-success">
+    <div class="content">
+        <p class="title">Здравствуй хозяина</p>
+        <p class="subtitle">With even more content</p>
+        <div class="content">
+            <!-- Content -->
+        </div>
+    </div>
+</article>
---
+++ templates/admin/orders.php
@@ -0,0 +1,8 @@
+<div id="orders">
+
+</div>
+<script>
+    var orders = new Orders('orders');
+    orders.render();
+</script>
+

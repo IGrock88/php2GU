db752de769b4a2fd42130f2065bb2dd1eb1b6287
IGrock88 <igrock88@gmail.com>
2018-03-05 23:29:01 +0800

    admin model

---
+++ engine/Models/AdminModel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IGrock
+ * Date: 05.03.2018
+ * Time: 19:51
+ */
+
+namespace engine\Models;
+
+
+class AdminModel extends Model
+{
+    public function getOrders(){
+        $this->db->connect();
+        $result = $this->db->select("select * from orders");
+        $this->db->close();
+        return $result;
+    }
+}

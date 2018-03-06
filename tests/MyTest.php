697794084e9dcf7841857756492e88991313a2a5
IGrock88 <igrock88@gmail.com>
2018-03-07 07:51:09 +0800

    add tests

---
+++ tests/MyTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IGrock
+ * Date: 06.03.2018
+ * Time: 21:54
+ */
+
+namespace tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function myFirstTest(){
+        $this->assertEquals(1, 1);
+    }
+}

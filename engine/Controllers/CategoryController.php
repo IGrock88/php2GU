6ce3e11c30b31578e044986906c9c850fe1f26ac
IGrock88 <igrock88@gmail.com>
2018-03-17 13:11:57 +0800

    realized getting off data on products by category

--- engine/Controllers/CategoryController.php
+++ engine/Controllers/CategoryController.php
@@ -3,15 +3,27 @@
 namespace engine\Controllers;
 
 
+use engine\Models\GoodsModel;
+
 class CategoryController extends Controller
 {
+    private $categoryes = [
+      "men" => 2,
+      "women" => 1
+    ];
+
+
     public function man(){
         $this->content['content'] = 'pages/category.tmpl';
+        $goodsModel = new GoodsModel($this->db);
+        $this->content['goods'] = $goodsModel->getGoodsByCategory($this->categoryes['men']);
         $this->view->generate($this->content);
     }
 
     public function women(){
         $this->content['content'] = 'pages/category.tmpl';
+        $goodsModel = new GoodsModel($this->db);
+        $this->content['goods'] = $goodsModel->getGoodsByCategory($this->categoryes['women']);
         $this->view->generate($this->content);
     }
 }
--- engine/Models/GoodsModel.php
+++ engine/Models/GoodsModel.php
@@ -9,11 +9,12 @@ use engine\DB\DB;
 
 class GoodsModel extends Model
 {
+    private $goodsTable = "goods";
 
     public function getFeatureGoods($quantityGoods, $startIndex = 0)
     {
         $this->db->connect();
-        $result = $this->db->select("select * from goods order by view desc, date desc limit $startIndex, $quantityGoods;");
+        $result = $this->db->select("select * from $this->goodsTable order by view desc, date desc limit $startIndex, $quantityGoods;");
         $this->db->close();
         return $result;
     }
@@ -21,7 +22,7 @@ class GoodsModel extends Model
     public function getGoodsById($idGoods)
     {
         $this->db->connect();
-        $result = $this->db->select("select * from goods as g
+        $result = $this->db->select("select * from $this->goodsTable as g
                                           JOIN designer as d ON d.designer_id=g.designer
                                           JOIN goods_material as gm ON gm.id_product=g.id_product
                                           JOIN materials as m ON m.id_material=gm.id_material
@@ -29,6 +30,14 @@ class GoodsModel extends Model
                                           where g.id_product=$idGoods;");
         $this->db->close();
         return $result[0];
+    }
+
+    public function getGoodsByCategory($idCategory)
+    {
+        $this->db->connect();
+        $result = $this->db->select("select * from $this->goodsTable WHERE id_category=$idCategory");
+        $this->db->close();
+        return $result;
     }
 
 }
